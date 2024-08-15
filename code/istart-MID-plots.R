d1208aaa429ba2b4278bb9ff7ae9d9710d2285e8
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2024-08-15 15:09:23 -0400

    Updates with proper RT/Behavioral Motivation contrasts

--- code/istart-MID-plots.R
+++ code/istart-MID-plots.R
@@ -15,7 +15,7 @@ library("ggcorrplot")
 library("psych")
 
 # import data
-data <- read_xlsx("~/Desktop/projects/MID/total_df_n46.xlsx")
+data <- read_xlsx("~/Documents/Github/istart-mid-clean/code/total_df_n46.xlsx")
 
 ## Fig 2 B1: TEPS x Behavioral Motivation
 #scatter <- ggplot(data, aes(x=score_teps_ant,V_beta))+
@@ -96,7 +96,7 @@ model1 <- lm(data$ACT_VS_Hit_minus_Miss ~
                data$comp_RS * data$score_teps_ant)
 summary(model1)
 
-### RS x V_beta
+### RS x V_beta ****************************************************************
 # ACT (VS): Salience
 model1 <- lm(data$Act_Salience ~
                data$comp_RS * data$V_beta)
@@ -104,48 +104,48 @@ summary(model1)
 
 # ACT (VS): LG>SG [Significant RS*V_beta interaction]
 model1 <- lm(data$Act_LG_minus_SG ~
-               data$comp_RS * data$V_beta)# * data$score_teps_ant)
+               data$comp_RS * data$LG_SG_new)# * data$score_teps_ant)
 summary(model1)
 
-scatter <- ggplot(data, aes(x=V_beta, y=Act_LG_minus_SG, col=RS_split))+
+scatter <- ggplot(data, aes(x=LG_SG_new, y=Act_LG_minus_SG, col=RS_split))+
     geom_point()+
   geom_point(shape=1)+
   geom_smooth(method=lm, se=TRUE, fullrange=TRUE, linetype="dashed", fill="lightgray")+
-  labs(x="Behavioral Motivation",y="VS Activation (LG>SG)")+
+  labs(x="Behavioral Motivation (LG>SG)",y="VS Activation (LG>SG)")+
   stat_cor(method="pearson")
 scatter + scale_color_manual(values = c("black", "gray")) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "gray"))
 
 # OR
-scatter <- ggplot(data, aes(x=comp_RS, y=Act_LG_minus_SG, col=V_beta_split))+
-    geom_point()+
-  geom_point(shape=1)+
-  geom_smooth(method=lm, se=TRUE, fullrange=TRUE, linetype="dashed", fill="lightgray")+
-  labs(x="Reward Sensitivity",y="VS Activation (LG>SG)")+
-  stat_cor(method="pearson")
-scatter + scale_color_manual(values = c("black", "gray")) + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "gray"))
+#scatter <- ggplot(data, aes(x=comp_RS, y=Act_LG_minus_SG, col=V_beta_split))+
+#    geom_point()+
+#  geom_point(shape=1)+
+#  geom_smooth(method=lm, se=TRUE, fullrange=TRUE, linetype="dashed", fill="lightgray")+
+#  labs(x="Reward Sensitivity",y="VS Activation (LG>SG)")+
+#  stat_cor(method="pearson")
+#scatter + scale_color_manual(values = c("black", "gray")) + 
+#  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+#        panel.background = element_blank(), axis.line = element_line(colour = "gray"))
 
 # ACT (VS): LL>SL
 model1 <- lm(data$Act_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
 # ACT (VS): LG>Neut
 model1 <- lm(data$ACT_VS_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
 # ACT (VS): LL>Neut
 model1 <- lm(data$ACT_VS_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
 
 # ACT (VS): LG>LL
 model1 <- lm(data$ACT_VS_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
 # ACT (VS): Hit>Miss
@@ -197,27 +197,29 @@ summary(model1)
 
 # PPI (VS seed, VMPFC Target): LG>SG
 model1 <- lm(data$PPI_LG_minus_SG ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_SG_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): LL>SL
 model1 <- lm(data$PPI_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
-# PPI (VS seed, VMPFC Target): LG>Neut
+# Below: Main effect of behavior when we control for RS ************************
+# interaction effect RS*BM (LG>N) p=.0961, main effect LG>N p=.0460* ***********
+# PPI (VS seed, VMPFC Target): LG>Neut *****************************************
 model1 <- lm(data$PPI_vmpfc_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): LL>Neut
 model1 <- lm(data$PPI_vmpfc_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): LG>LL
 model1 <- lm(data$PPI_vmpfc_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): Hit>Miss
@@ -307,27 +309,27 @@ summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): LG>SG
 model1 <- lm(data$DMN_VS_LG_minus_SG ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_SG_new)
 summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): LL>SL
 model1 <- lm(data$DMN_VS_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): LG>Neut
 model1 <- lm(data$DMN_VS_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): LL>Neut
 model1 <- lm(data$DMN_VS_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): LG>LL
 model1 <- lm(data$DMN_VS_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
 # nPPI (DMN seed, VMPFC Target): Hit>Miss
@@ -591,27 +593,27 @@ summary(model1)
 
 # ACT (VS): LG>SG
 model1 <- lm(data$zstat_ACT_VS_LG_minus_SG ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_SG_new)
 summary(model1)
 
 # ACT (VS): LL>SL
 model1 <- lm(data$zstat_ACT_VS_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
 # ACT (VS): LG>Neut
 model1 <- lm(data$zstat_ACT_VS_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
 # ACT (VS): LL>Neut
 model1 <- lm(data$zstat_ACT_VS_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
 
 # ACT (VS): LG>LL
 model1 <- lm(data$zstat_ACT_VS_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
 # ACT (VS): Hit>Miss
@@ -665,27 +667,39 @@ summary(model1)
 
 # PPI (VS seed, VMPFC Target): LG>SG
 model1 <- lm(data$zstat_PPI_vmpfc_LG_minus_SG ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_SG_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): LL>SL [Main effect of RS]
 model1 <- lm(data$zstat_PPI_vmpfc_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): LG>Neut
 model1 <- lm(data$zstat_PPI_vmpfc_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
-# PPI (VS seed, VMPFC Target): LL>Neut
+# PPI (VS seed, VMPFC Target): LL>Neut *****************************************
+# Significant interaction RS*LL>N p=.041 ***************************************
 model1 <- lm(data$zstat_PPI_vmpfc_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
+
+scatter <- ggplot(data, aes(x=LL_N_new, y=zstat_PPI_vmpfc_LL_minus_N, col=RS_split))+
+  geom_point()+
+  geom_point(shape=1)+
+  geom_smooth(method=lm, se=TRUE, fullrange=TRUE, linetype="dashed", fill="lightgray")+
+  labs(x="Behavioral Motivation (LL>N)",y="VS-vmPFC (LL>N) (zstat)")+
+  stat_cor(method="pearson")
+scatter + scale_color_manual(values = c("black", "gray")) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "gray"))
+
 
 # PPI (VS seed, VMPFC Target): LG>LL
 model1 <- lm(data$zstat_PPI_vmpfc_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
 # PPI (VS seed, VMPFC Target): Hit>Miss
@@ -748,32 +762,45 @@ scatter + scale_color_manual(values = c("black", "gray")) +
         panel.background = element_blank(), axis.line = element_line(colour = "gray"))
 
 
-# nPPI (DMN seed, VMPFC Target): LG>SG
+# nPPI (DMN seed, VS Target): LG>SG
+# Slight main effect of behavioral motivation (LG>SG) when controlling for RS p=.047
 model1 <- lm(data$zstat_DMN_VS_LG_minus_SG ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_SG_new)
 summary(model1)
 
-# nPPI (DMN seed, VMPFC Target): LL>SL
+# nPPI (DMN seed, VS Target): LL>SL
 model1 <- lm(data$zstat_DMN_VS_LL_minus_SL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_SL_new)
 summary(model1)
 
-# nPPI (DMN seed, VMPFC Target): LG>Neut
+# nPPI (DMN seed, VS Target): LG>Neut ******************************************
+# Highly significant interaction this is the way ******************************* See this result
+# Might want to plot this the other way ****************************************
 model1 <- lm(data$zstat_DMN_VS_LG_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_N_new)
 summary(model1)
 
-# nPPI (DMN seed, VMPFC Target): LL>Neut
+scatter <- ggplot(data, aes(x=LG_N_new, y=zstat_DMN_VS_LG_minus_N, col=RS_split))+
+  geom_point()+
+  geom_point(shape=1)+
+  geom_smooth(method=lm, se=TRUE, fullrange=TRUE, linetype="dashed", fill="lightgray")+
+  labs(x="Behavioral Motivation (LG>N)",y="DMN-VS (LG>N) (zstat)")+
+  stat_cor(method="pearson")
+scatter + scale_color_manual(values = c("black", "gray")) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "gray"))
+
+# nPPI (DMN seed, VS Target): LL>Neut
 model1 <- lm(data$zstat_DMN_VS_LL_minus_N ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LL_N_new)
 summary(model1)
 
-# nPPI (DMN seed, VMPFC Target): LG>LL
+# nPPI (DMN seed, VS Target): LG>LL
 model1 <- lm(data$zstat_DMN_VS_LG_minus_LL ~
-               data$comp_RS * data$V_beta)
+               data$comp_RS * data$LG_LL_new)
 summary(model1)
 
-# nPPI (DMN seed, VMPFC Target): Hit>Miss
+# nPPI (DMN seed, VS Target): Hit>Miss
 model1 <- lm(data$zstat_DMN_VS_Hit_minus_Miss ~
                data$comp_RS * data$V_beta)
 summary(model1)
